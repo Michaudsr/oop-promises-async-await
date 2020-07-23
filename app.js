3c135a6ef97161161356405f429dea18d3059419
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-07-23 17:50:51 -0400

    [constructor] constructor function

--- app.js
+++ app.js
@@ -39,4 +39,38 @@ catTwo.__proto__ = cat;
 console.log(cat);
 console.log(catTwo);
 
-//
+// Constructor Functions
+
+function User(name) {
+    // this
+    this.name = name; 
+
+    // return this
+}
+
+let adam = new User('Adam');
+console.log(adam.name);
+let pete = new User('Pete');
+console.log(pete.name);
+
+function NBAPlayer(name, team, threePointShooter) {
+    this.name = name;
+    this.team = team;
+    this.threePointShooter = threePointShooter;
+}
+
+let steph = new NBAPlayer('Steph Curry', 'Warriors', true);
+console.log(steph);
+console.log(steph.name);
+// make a constructor function
+// make 3 new variables with that contructor function
+
+function TruckType( brand, model , true) {
+    this.brand = brand;
+    this.model = model;
+    this.bestModel = bestModel;
+}
+
+let chevy = new TruckType('Chevrolet', 'Silverado', true);
+console.log(chevy);
+console.log(chevy.brand);
