5fa6a0bb4812798f7731d49b795ce110e93bc55f
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-07-23 18:57:00 -0400

    [class] add github profile

--- app.js
+++ app.js
@@ -57,11 +57,17 @@ function NBAPlayer(name, team, threePointShooter) {
     this.name = name;
     this.team = team;
     this.threePointShooter = threePointShooter;
+    this.intro = function(){
+        console.log("Hi, my name is " + this.name)
+    }
 }
 
 let steph = new NBAPlayer('Steph Curry', 'Warriors', true);
 console.log(steph);
 console.log(steph.name);
+steph.intro();
+
+
 // make a constructor function
 // make 3 new variables with that contructor function
 
@@ -69,10 +75,66 @@ function TruckType(brand, model, bestModel) {
     this.brand = brand;
     this.model = model;
     this.bestModel = bestModel;
+    this.intro = function(){
+        console.log("Hi, my name is" + this.name)
+    }
 }
 
 let chevy = new TruckType('Chevrolet', 'Silverado', true);
 console.log(chevy);
 console.log(chevy.brand);
+
+// Class
+// class Car {
+//     constructor(year, make, model, color, ) {
+//         this.year = year;
+//         this.make = make;
+//         this.model = model;
+//         this.color = color;
+//     }
+    
+// }
+//         drive() {
+//             console.log('Vroom');
+//         }
+    
+    
+
+
+
+// let tesla = new Car(2020, 'Tesla', 'Model S', 'red' );
+// console.log(tesla);
+// tesla.drive();
+// tesla.intro();
+
+class GithubProfile {
+    constructor(username, name, url) {
+    this.username = username;
+    this.name = name;
+    this.url = url
+}
+
+intro(){
+    console.log(`My name is ${this.name}and my username is @${this.username} `);
+}
+}
+// 
+fetch('https://api.github.com/users/michaudsr')
+.then(response =>{
+    return response.json();
+})
+
+.then( data => {
+    
+    let githubURL = data.url;
+    let githubUsername = data.login;
+    let githubName = data.name;
+
+    let steven = new GithubProfile(githubUsername, githubName, githubURL);
+    console.log(steven);
+
+})
+
+
 
 
